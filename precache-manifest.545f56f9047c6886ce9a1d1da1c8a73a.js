c67da6dd6f62344b76c83937f96586d9e766443a
kowalskiadrian <kowalski.adrian.adam@outlook.com>
2020-09-07 14:09:07 +0200

    Updates

---
+++ precache-manifest.545f56f9047c6886ce9a1d1da1c8a73a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b68b4edff005eac513b4cba3598f4f",
+    "url": "/gamewave/index.html"
+  },
+  {
+    "revision": "45d60d119a7c2d0b6483",
+    "url": "/gamewave/static/css/main.d796b3ba.chunk.css"
+  },
+  {
+    "revision": "f4b510c34a55fd5a7b4a",
+    "url": "/gamewave/static/js/2.2691c0c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gamewave/static/js/2.2691c0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d60d119a7c2d0b6483",
+    "url": "/gamewave/static/js/main.05ecd2f3.chunk.js"
+  },
+  {
+    "revision": "88db975f4ebfc234a5f6",
+    "url": "/gamewave/static/js/runtime-main.32d7cbbf.js"
+  },
+  {
+    "revision": "66038e757b24ca3342a0e81e2afcadf8",
+    "url": "/gamewave/static/media/pokeball.66038e75.svg"
+  }
+]);
